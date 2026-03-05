622af6bd9f2383cf51599fbe41c48081c42c1da0
Tamal Mallick <tamalmallick23@gmail.com>
2026-03-05 11:21:33 +0530

    Test Cases Passed 20 / 20 | Time Taken 0.16

---
+++ Generate All Binary Strings/Solution.py
@@ -0,0 +1,18 @@
+class Solution:
+
+    def solve(self, index, flag, numbers, result):
+        if index == len(numbers):
+            result.append("".join(numbers))
+            return
+        
+        numbers[index] = "0"
+        self.solve(index + 1, True, numbers, result)
+
+        numbers[index] = "1"
+        self.solve(index + 1, True, numbers, result)
+
+    def binstr(self, n):
+        numbers = ["0"] * n
+        result = []
+        self.solve(0, True, numbers, result)
+        return result

8524f8803f908dca9b7d60064dcb41ba27bbafcb
Tamal Mallick <tamalmallick23@gmail.com>
2026-04-04 12:24:40 +0530

    Test Cases Passed 1116 / 1116 | Time Taken 0.37

---
+++ Next Greater Element/Solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def nextLargerElement(self, arr):
+        n=len(arr)
+        stack=[]
+        ans=[-1]*n
+        for i in range(n-1,-1,-1):
+            while len(stack)!=0 and stack[-1] <= arr[i]:
+                stack.pop()
+            if len(stack)!=0:
+                ans[i]=stack[-1]
+            stack.append(arr[i])
+        return ans
+        
